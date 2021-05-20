303fb6a7041de081309d847a3cc353711d115ab7
gparkkii <jyp933@gmail.com>
2021-05-20 19:10:09 +0900

    Geunu's Blog Project

---
+++ gatsby-meta-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  title: `Geunu.io`,
+  description: `Blog posted about Geunu's Devlog ...`,
+  author: `Geunwoo Baek`,
+  introduction: `I explain with words and code.`,
+  siteUrl: `https://geunu.github.io`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: ``, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'geunwoobaek/geunu.github.io', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+}
